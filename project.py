95fe4286046b47904149a320644926cbd9afa3a3
Sneha-Srivastava11 <149507837+Sneha-Srivastava11@users.noreply.github.com>
2025-04-12 00:00:55 +0530

    Update project.py

--- project.py
+++ project.py
@@ -14,3 +14,26 @@ plt.figure(figsize=(10,6))
 sns.heatmap(corr,annot=True,cmap='coolwarm')
 plt.title("Correlation of all the blood test")
 plt.show()
+'''Objective 1->To find out which blood tests change the most as Hepatitis C becomes more serious,
+and how those test results look in patients with cirrhosis.'''
+
+# Cleaning column names
+data.columns = data.columns.str.strip()
+
+# Print unique categories to see disease stages
+print("Disease categories:", data['Category'].unique())
+
+# Focus on main liver test results
+tests = ['ALT', 'AST', 'BIL']
+
+# Group by disease category and calculate mean values
+grouped_means = data.groupby('Category')[tests].mean()
+
+# Plot
+grouped_means.plot(kind='bar', figsize=(10,6))
+plt.title('Average ALT, AST, BIL Values by Disease Stage')
+plt.ylabel('Average Test Value')
+
+plt.grid(True)
+plt.tight_layout()
+plt.show()
